f90d4e1ec53321f43784ae2421fa2b4ad980890b
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2019-05-06 21:38:49 +0700

    Initial Commit

---
+++ PengukurBeratIdeal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PengukurBeratIdeal
+//
+//  Created by Bill Tanthowi Jauhari on 06/05/19.
+//  Copyright © 2019 Batavia Hack Town. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
